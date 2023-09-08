499582d869f33acaf427b5772cb17607cd378987
Komalgiri <komalgiri789@gmail.com>
2023-09-08 22:37:48 +0530

    QuizApp

---
+++ app/src/main/java/com/example/quizapp/QuestionAnswer.java
@@ -0,0 +1,25 @@
+package com.example.quizapp;
+
+public class QuestionAnswer {
+    public static String question[] = {
+            "Which one is the first search engine in internet?",
+            "Which one the first web browser invented in 1990?",
+            "Which programming language used to create program like applets?",
+            "First computer virus is known as.",
+            "Firewall in computer is used for."
+    };
+    public static String choices[][] = {
+            {"Google","Archie","Altavista","WAIS"},
+            {"Internet Explorer","Mosaic","Mozilla","Nexus"},
+            {"COBOL","C language","Java","BASIC"},
+            {"Rabbit","Creeper virus","ELK cloner","SCA virus"},
+            {"Security","Data transmission","Authentication","Monitoring"}
+    };
+    public static String correctAnswer[] = {
+            "Archie",
+            "Nexus",
+            "Java",
+            "Creeper virus",
+            "Security"
+    };
+}
